913bb0bde808b542d30f8e74b53986462b23bc3b
DominikaKawalec <dominika.kawalec97@gmail.com>
2024-03-03 16:00:53 +0100

    add app final

--- movieLibrary/src/main/java/org/example/MovieLibraryApp.java
+++ movieLibrary/src/main/java/org/example/MovieLibraryApp.java
@@ -1,10 +1,27 @@
 package org.example;
 
+import org.example.handlers.MenuHandlers;
+import org.example.handlers.UserInputHandler;
 import org.example.models.MovieLibrary;
 
 public class MovieLibraryApp {
     public static void main(String[] args) {
         MovieLibrary movieLibrary = new MovieLibrary();
         movieLibrary.fillMovieList();
+        MenuHandlers.print();
+        while (true) {
+            switch (UserInputHandler.getMenuOptions()) {
+                case DISPLAYINFORMATIONABOUTRANDOMMOVIE:
+                    movieLibrary.getRandomMovieInformation();
+                    break;
+                case DISPLAYALLMOVIESFORACTOR:
+                    movieLibrary.getFilmsForActor();
+                    break;
+                case QUIT:
+                    System.out.println("Wychodzenie");
+                    System.exit(1);
+            }
+
+        }
     }
 }
